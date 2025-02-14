c489754aea228c60bfbd4a031dad2cc711daf25c
Eduardo Moron <eduardo.moron@swissbytes.ch>
2025-02-13 23:51:24 -0400

    added support for odoo 18

--- static/src/js/receipt_screen_extension.js
+++ static/src/js/receipt_screen_extension.js
@@ -1,34 +1,39 @@
 /** @odoo-module **/
 
 import {patch} from "@web/core/utils/patch";
-import {session} from "@web/session";
-import {useService} from "@web/core/utils/hooks";
-import {ReceiptScreen} from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
-import {formatDateTime,} from "@web/core/l10n/dates";
+import {formatDateTime, serializeDateTime} from "@web/core/l10n/dates";
+const { DateTime } = luxon;
 
+import {PosStore} from "@point_of_sale/app/store/pos_store";
 
-patch(ReceiptScreen.prototype, {
+patch(PosStore.prototype, {
 
-    setup() {
-        super.setup();
-        this.orm = useService("orm");
-        this.notification = useService("notification");
+    async setup(){
+      super.setup(...arguments);
     },
-
-    async printReceipt() {
-        const result = await this.orm.call('res.users', 'search_read', [], {
-            domain: [['id', '=', session.uid]],
+    async printReceipt({
+                           basic = false,
+                           order = this.get_order(),
+                           printBillActionTriggered = false,
+                       } = {}) {
+        console.log("SEND TO PRINTER");
+        const result = await this.env.services.orm.call('res.users', 'search_read', [], {
+            domain: [['id', '=', this.session.user_id.id]],
             fields: ['printer_url'],
         })
         let printerUrl = result[0].printer_url;
+        console.log("to:", printerUrl);
         let jsonForPrinter = this._prepareJsonForPrinter();
         console.log(jsonForPrinter);
         await this.sendToPrinter(printerUrl, jsonForPrinter);
+        return true;
     },
+
+
 
     async sendToPrinter(printerUrl, jsonForPrinter) {
         try {
-            const response = await this.fetchWithTimeout(printerUrl, {
+            const response = await this._fetchWithTimeout(printerUrl, {
                 headers: {
                     "Content-type": "application/json",
                 },
@@ -41,14 +46,15 @@ patch(ReceiptScreen.prototype, {
 
         } catch (error) {
             console.error('Error al enviar a la impresora:', error.message);
-            this.notification.add(
+            alert('Error al enviar a la impresora. ' + error.message);
+            this.env.services.notification.add(
                 'Error al enviar a la impresora. ' + error.message,
                 {type: 'danger'},
             )
         }
     },
 
-    fetchWithTimeout(url, options = {}, timeout = 5000) {
+    _fetchWithTimeout(url, options = {}, timeout = 5000) {
         const timeoutPromise = new Promise((_, reject) =>
             setTimeout(() => reject(new Error('La solicitud ha tardado demasiado y ha sido cancelada.')), timeout)
         );
@@ -60,28 +66,28 @@ patch(ReceiptScreen.prototype, {
     },
 
     _prepareJsonForPrinter() {
-        const order = this.currentOrder;
+        const order = this.get_order();
         const orderTotalAmount = order.get_total_with_tax();
         const orderAmountStr = this._formatCurrencyForPrinter(orderTotalAmount);
-        const dateOrder = formatDateTime(order.date_order);
+        const dateOrder = formatDateTime(DateTime.fromFormat(order.date_order, "yyyy-MM-dd HH:mm:ss"));
         let formattedLines = this._prepareLinesJsonForPrinter();
         let formattedPaymentLines = this._preparePaymentLinesJsonForPrinter();
 
         return [
             {
                 'type': 'text',
-                'text': this.pos.company.name,
+                'text': this.get_order().company_id.name,
                 'styles': {
                     'align': 'center', 'bold': true
                 }
             },
             {
                 'type': 'text',
-                'text': 'Empleado:' + this.currentOrder.cashier.name,
+                'text': 'Empleado:' + this.get_order().user_id.name,
             },
             {
                 'type': 'text',
-                'text': this.currentOrder.name,
+                'text': this.get_order().name,
             },
             {
                 'type': 'text',
@@ -126,7 +132,7 @@ patch(ReceiptScreen.prototype, {
     },
 
     _prepareLinesJsonForPrinter() {
-        const orderLines = this.currentOrder.get_orderlines();
+        const orderLines = this.get_order().get_orderlines();
         let formattedOrderLines = [];
         orderLines.map(line => {
             formattedOrderLines.push({
@@ -134,7 +140,7 @@ patch(ReceiptScreen.prototype, {
                 'columns': [
                     {
                         'type': 'text',
-                        'text': line.product.display_name,
+                        'text': line.product_id.display_name,
                         'width': 6
                     },
                     {
@@ -167,13 +173,13 @@ patch(ReceiptScreen.prototype, {
 
     _preparePaymentLinesJsonForPrinter() {
         let formattedPaymentLines = [];
-        this.currentOrder.get_paymentlines().map(line => {
+        this.get_order().payment_ids.map(line => {
             formattedPaymentLines.push({
                 'type': 'rows',
                 'columns': [
                     {
                         'type': 'text',
-                        'text': line.payment_method.name,
+                        'text': line.payment_method_id.name,
                         'width': 6
                     },
                     {
@@ -195,5 +201,5 @@ patch(ReceiptScreen.prototype, {
         return amountWithCurrency.replace(/\s+/g, "");
     }
 
-
 });
+
